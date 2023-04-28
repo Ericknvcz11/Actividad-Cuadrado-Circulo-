6a4c6f209de30bb49b5b78c2cc13b68b180c5ee1
Ericknvcz11 <122307598+Ericknvcz11@users.noreply.github.com>
2023-04-27 20:14:36 -0600

    Add files via upload

---
+++ Main3.java
@@ -0,0 +1,17 @@
+
+public class Main3 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Cuadrado3 A = new Cuadrado3(); 
+			System.out.println(A.calcularArea()); 
+		Cuadrado3 B = new Cuadrado3 ();
+			System.out.println(B.calcularPerimetro()); 
+		Cuadrado3 C = new Cuadrado3(); 
+			System.out.println(C.calcularDiagonal()); 
+		Cuadrado3 D = new Cuadrado3 (); 
+			System.out.println(D.calcularArea()); 
+			//D.cambiarLongitud(1);
+	}
+
+}
